4b288a76dd69d131e089dd0a10e0b2f6f4fbc147
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-17 15:41:52 +0800

    Update deletetoy.php

---
+++ pages/deletetoy.php
@@ -0,0 +1,30 @@
+<?php require_once 'main/dbConfig.php'; ?>
+<?php require_once 'main/models.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<?php $getToyByID = getToyByID($pdo, $_GET['toy_id']); ?>
+	<h1>Are you sure you want to delete this toy?</h1>
+	<div class="container" style="border-style: solid; height: 400px;">
+		<h2>toy Name: <?php echo $getToyByID['toy_name'] ?></h2>
+		<h2>Technologies Used: <?php echo $getToyByID['toy_type'] ?></h2>
+		<h2>toy Owner: <?php echo $getToyByID['toy_owner'] ?></h2>
+		<h2>Date Added: <?php echo $getToyByID['date_added'] ?></h2>
+
+		<div class="deleteBtn" style="float: right; margin-right: 10px;">
+
+			<form action="main/handleForms.php?toy_id=<?php echo $_GET['toy_id']; ?>&toy_reseller_id=<?php echo $_GET['toy_reseller_id']; ?>" method="POST">
+				<input type="submit" name="deletetoyBtn" value="Delete">
+			</form>			
+			
+		</div>	
+
+	</div>
+</body>
+</html>
